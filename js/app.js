c28d200e8f5368ca25d0ff830c0d323e178a79bf
adriavallesbaradad <adriavallesbaradad@gmail.com>
2022-05-09 19:21:53 +0200

    create data item array

--- js/app.js
+++ js/app.js
@@ -4,9 +4,15 @@ allBtnFilter.addEventListener("click", getCategory);
 function getCategory(e) {
     e.preventDefault();
     const category = e.target.dataset.filter;
-    
+
     if (!category) {
         return
     }
-    
+const cardList = Array.from(document.querySelectorAll('[data-item]'));
+// filter
+// for
+// map
+
+
 }
+
