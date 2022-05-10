ce380b6b809cc515826f73ffec9adfc63992427f
Sofia Romera <sofia-romera@outlook.com>
2022-05-10 18:25:49 +0200

    Create function FilteredCards

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const allBtnFilter = document.getElementById("btnFilter");
 allBtnFilter.addEventListener("click", getCategory);
+const cardList = Array.from(document.querySelectorAll('[data-item]'));
+
+// cardList.map(card => {
+//     console.log(card.dataset.item);
+// });
 
 function getCategory(e) {
     e.preventDefault();
@@ -8,11 +13,21 @@ function getCategory(e) {
     if (!category) {
         return
     }
-const cardList = Array.from(document.querySelectorAll('[data-item]'));
-// filter
-// for
-// map
 
-
+    filterCards(category);
 }
 
+function filterCards(category){
+    let filteredCards = cardList.filter(card => {
+        if(card.dataset.item === category){
+            return card
+        }
+    })
+    cardList.forEach(card => {
+        card.classList.toggle('hidden');
+    })
+    filteredCards.forEach(card => {
+        card.classList.toggle('hidden');
+    })
+    
+}
